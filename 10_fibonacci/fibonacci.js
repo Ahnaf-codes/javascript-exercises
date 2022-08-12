4b3c46ce885e07d5258aa91383c3a7c2924819c9
Ahnaf <ahnafanis0@gmail.com>
2022-08-12 21:18:45 +0600

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
+    const fib = [0, 1];
 
+    for (i = 2; i <= n; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    console.log(fib[n]);
 };
 
+fibonacci();
 // Do not edit below this line
 module.exports = fibonacci;
