9c038b90ad6856f70b7ca8cb5e65641b433cc466
Ahnaf <ahnafanis0@gmail.com>
2022-08-12 18:45:17 +0600

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = (str) => {
+    const processedStr = str.toLowerCase().replaceAll(" ", "");
+    const reverseStr = [...processedStr].reverse().join("");
 
+    return reverseStr === processedStr;
 };
 
+palindromes("A nut for a jar of tuna");
 // Do not edit below this line
 module.exports = palindromes;
