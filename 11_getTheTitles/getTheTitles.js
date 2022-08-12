b0bd3646dba873cd698e93e26dd4ae83f0bd7241
Ahnaf <ahnafanis0@gmail.com>
2022-08-12 22:40:14 +0600

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+    const titles = [];
+    for (let i = 0; i < arr.length; i++) {
+        const title = arr[i].title;
+        titles.push(title);
+    }
+    console.log(titles);
 };
 
+getTheTitles([
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]);
 // Do not edit below this line
 module.exports = getTheTitles;
